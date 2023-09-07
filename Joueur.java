4f737b338b55a325e3207b83cbf8362933ee6b04
Tounzitoun <fael.@outlook.fr>
2023-09-07 12:30:18 +0200

    class joueur

--- Joueur.java
+++ Joueur.java
@@ -1,2 +1,9 @@
-public class Joueur {
+public abstract class Joueur {
+    private String nom;
+    private Integer score;
+
+    public Joueur(String nom){
+        this.nom = nom;
+        this.score = 0;
+    }
 }
