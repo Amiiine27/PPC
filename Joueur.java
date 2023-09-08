4d0e7e3b53bdb59c6beded0e4d8390a92142b6d1
Tounzitoun <fael.@outlook.fr>
2023-09-08 10:02:03 +0200

    fichier test

--- Joueur.java
+++ Joueur.java
@@ -1,9 +1,37 @@
 public abstract class Joueur {
     private String nom;
     private Integer score;
+    private Coup coupCourant;
 
     public Joueur(String nom){
         this.nom = nom;
         this.score = 0;
     }
+
+    public Coup getCoupCourant() {
+        return coupCourant;
+    }
+
+    public void setCoupCourant(Coup coupCourant) {
+        this.coupCourant = coupCourant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    public abstract void choisitUncoup();
+
+    @Override
+    public String toString() {
+        return "Joueur{" +
+                "nom='" + nom + '\'' +
+                ", score=" + score +
+                ", coupCourant=" + coupCourant +
+                '}';
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
 }
